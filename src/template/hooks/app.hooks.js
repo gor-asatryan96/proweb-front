0a9f0be81e34bd99f077771c571d5aedc7d3b14b
Lev <tovmasyan.lev@gmail.com>
2022-05-11 15:52:49 +0400

    feat: useAppSideEffect custom hook

--- src/template/hooks/app.hooks.js
+++ src/template/hooks/app.hooks.js
@@ -4,12 +4,11 @@ import { useSearchParams } from 'react-router-dom';
 import { openPopup } from '../../redux/slices/popups.slice';
 import { setIsConnected } from '../../redux/slices/user.slice';
 import { getUserByTokenThunk } from '../../redux/thunks/auth.thunk';
-import { setGifts } from '../../redux/slices/gifts.slice';
-import { GIFTS } from '../components/Popups/UserProfilePopup/components/Gifts/components/GiftInfo/constants/giftInfo.constants';
 import { messagesThunk } from '../../redux/thunks/messages.thunk';
 import { DESTINATION_CONFIGS } from '../components/Popups/configs/destination.configs';
+import { getServerConfigsThunk } from '../../redux/thunks/serverConfigs.thunk';
 
-export const useSideEffects = () => {
+export const useAppSideEffects = () => {
   const dispatch = useDispatch();
   const [ params ] = useSearchParams();
 
@@ -18,6 +17,10 @@ export const useSideEffects = () => {
     if (destination && DESTINATION_CONFIGS[destination]) {
       dispatch(openPopup(DESTINATION_CONFIGS[destination]));
     }
+  }, []);
+
+  useEffect(() => {
+    dispatch(getServerConfigsThunk());
   }, []);
 
   useEffect(() => {
