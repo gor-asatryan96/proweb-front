58a66306ad4ffead4ec44e3dd509f7cbd917d19c
Lev <tovmasyan.lev@gmail.com>
2022-05-11 16:00:43 +0400

    feat: prematch skeleton loaders

---
+++ src/template/pages/Sportsbook/Prematch/components/PrematchMenu/components/PrematchMenuItem/PrematchMenuItemSkeleton.jsx
@@ -0,0 +1,32 @@
+import { useMediaQuery } from 'react-responsive';
+import ContentLoader from 'react-content-loader';
+import { MEDIA_QUERIES } from '../../../../../../../../constants/mediaQuery.constants';
+
+const PrematchMenuItemSkeleton = () => {
+  const isDesktop = useMediaQuery(MEDIA_QUERIES.DESKTOP);
+
+  return (
+    <div className='sidebar__item sidebar__item_skeleton'>
+      <ContentLoader
+        speed={2}
+        width='100%'
+        height='100%'
+        backgroundColor="var(--pmbet-blue-dark)"
+        foregroundColor='var(--pmbet-blue-darker)'
+        >
+        <circle
+          cx={isDesktop ? '8%' : '50%'}
+          cy={isDesktop ? '50%' : '37%'}
+          r={isDesktop ? '7%' : '17%'} />
+        <rect
+          x={isDesktop ? '19%' : '15%'}
+          y={isDesktop ? '37%' : '70%'}
+          width={isDesktop ? '30%' : '70%'}
+          height={isDesktop ? '26%' : '18%'} />
+        {isDesktop && <rect x="88%" y="37%" width="6%" height="26%" />}
+      </ContentLoader>
+    </div>
+  );
+};
+
+export default PrematchMenuItemSkeleton;
