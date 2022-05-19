f6d914cd4a6d68b4c5eec6f5e34d28940889dcef
ermusaelyan <e.musaelyan98@gmail.com>
2022-05-19 19:04:05 +0400

    Sidebar desktop sport page styles upd

--- src/template/pages/Sportsbook/Sport/components/SportMenu/SportMenu.jsx
+++ src/template/pages/Sportsbook/Sport/components/SportMenu/SportMenu.jsx
@@ -13,20 +13,91 @@ const PrematchMenu = () => {
 
   return (
     <section className={classNames('sidebar', { sidebar__loading: isSportsLoading })}>
-      <ul className="sidebar__list">
-        {isSportsLoading
-          ? LOADING_ITEMS.map((_, i) => (
-            <PrematchMenuItemSkeleton key={i} />
-          ))
-          : sportsList.map(sport => (
-            <PrematchMenuItem
-              key={sport.id}
-              data={sport}
-              isActive={sportName === sport.sportName}
-            />
-          ))
-        }
-      </ul>
+      <div className="sidebar__betType">
+        <ul className="sidebar__betTypeList">
+          <li className="sidebar__betTypeItem active">
+            <div className="sidebar__betTypeInner">
+              <button type="button" className="sidebar__betTypeButton">
+                <div className="sidebar__betTypeIcon">
+                  <svg className="sidebar__betTypeSvg">
+                    <use xlinkHref="#hotsTops" />
+                  </svg>
+                </div>
+                <div className="sidebar__betTypeName">HOTS & TOPS</div>
+              </button>
+            </div>
+          </li>
+          <li className="sidebar__betTypeItem">
+            <div className="sidebar__betTypeInner">
+              <button type="button" className="sidebar__betTypeButton">
+                <div className="sidebar__betTypeIcon">
+                  <svg className="sidebar__betTypeSvg">
+                    <use xlinkHref="#expressBet" />
+                  </svg>
+                </div>
+                <div className="sidebar__betTypeName">Express Bet</div>
+              </button>
+            </div>
+          </li>
+          <li className="sidebar__betTypeItem">
+            <div className="sidebar__betTypeInner">
+              <button type="button" className="sidebar__betTypeButton">
+                <div className="sidebar__betTypeIcon">
+                  <svg className="sidebar__betTypeSvg">
+                    <use xlinkHref="#boostedOdds" />
+                  </svg>
+                </div>
+                <div className="sidebar__betTypeName">boosted Odds</div>
+              </button>
+            </div>
+          </li>
+          <li className="sidebar__betTypeItem active">
+            <div className="sidebar__betTypeInner">
+              <button type="button" className="sidebar__betTypeButton">
+                <div className="sidebar__betTypeIcon">
+                  <svg className="sidebar__betTypeSvg">
+                    <use xlinkHref="#pre-match" />
+                  </svg>
+                </div>
+                <div className="sidebar__betTypeName">pre-match</div>
+              </button>
+            </div>
+            <div className="sidebar__betTypeTime">
+              <div className="sidebar__betTypeHour">
+                <button type="button" className="sidebar__betTypeTimeButton active">All</button>
+              </div>
+              <div className="sidebar__betTypeHour">
+                <button type="button" className="sidebar__betTypeTimeButton">3 hrs</button>
+              </div>
+              <div className="sidebar__betTypeHour">
+                <button type="button" className="sidebar__betTypeTimeButton">6 hrs</button>
+              </div>
+              <div className="sidebar__betTypeHour">
+                <button type="button" className="sidebar__betTypeTimeButton">9 hrs</button>
+              </div>
+              <div className="sidebar__betTypeHour">
+                <button type="button" className="sidebar__betTypeTimeButton">12 hrs</button>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="sidebar__content">
+        <ul className="sidebar__list">
+          {isSportsLoading
+            ? LOADING_ITEMS.map((_, i) => (
+              <PrematchMenuItemSkeleton key={i} />
+            ))
+            : sportsList.map(sport => (
+              <PrematchMenuItem
+                key={sport.id}
+                data={sport}
+                isActive={sportName === sport.sportName}
+                        />
+            ))
+                }
+        </ul>
+      </div>
     </section>
   );
 };
--- src/template/pages/Sportsbook/Sport/components/SportMenu/components/PrematchMenuItem/PrematchMenuItem.jsx
+++ src/template/pages/Sportsbook/Sport/components/SportMenu/components/PrematchMenuItem/PrematchMenuItem.jsx
@@ -8,6 +8,7 @@ import { selectServerImageUrl } from '../../../../../../../../redux/reducers/ser
 import BallSkeleton from './BallSkeleton';
 import { replaceSpaces } from '../../../../../../../../helpers/utils';
 import { SPORT_TABS_URLS } from '../../../../constants/sport.constants';
+import Flag from '../../../../../../../../components/Flag/Flag';
 
 const { PRE_MATCH } = SPORT_TABS_URLS;
 
@@ -15,37 +16,94 @@ const PrematchMenuItem = ({ data, isActive }) => {
   const navigate = useNavigate();
   const serverImageUrl = useSelector(selectServerImageUrl);
   const [ isImageLoaded, setIsImageLoaded ] = useState(false);
+  const [ isOpen, setIsOpen ] = useState(false);
+  const [ isSelected, setIsSelected ] = useState(false);
 
   const isDesktop = useMediaQuery(MEDIA_QUERIES.DESKTOP);
 
   const onSportClick = () => {
-    if (isActive) return;
-    navigate(`/Sport/${PRE_MATCH}/${replaceSpaces(data.sportName)}/${replaceSpaces(data.countries[0].name)}/${
-      replaceSpaces(data.countries[0].leagues[0].competitionName)}`);
+    // if (isActive) return;
+    // navigate(`/Sport/${PRE_MATCH}/${replaceSpaces(data.sportName)}/${replaceSpaces(data.countries[0].name)}/${
+    //   replaceSpaces(data.countries[0].leagues[0].competitionName)}`);
+    setIsOpen(prevState => !prevState);
+  };
+  const onCountryClick = () => {
+    // if (isActive) return;
+    // navigate(`/Sport/${PRE_MATCH}/${replaceSpaces(data.sportName)}/${replaceSpaces(data.countries[0].name)}/${
+    //   replaceSpaces(data.countries[0].leagues[0].competitionName)}`);
+    setIsSelected(prevState => !prevState);
   };
 
   const sportIcon = serverImageUrl && `${serverImageUrl}/sports/${data.sportId}.svg`;
 
   return (
     <li
-      onClick={onSportClick}
       className={classNames('sidebar__item', { 'sidebar__item--active': isActive })}>
-      <button className="sidebar__btn">
-        <span className="sidebar__icon">
-          {!isImageLoaded && <div className='sidebar__icon_skeleton'><BallSkeleton /></div>}
-          <span className="img-container">
-            <img src={sportIcon} alt='' onLoad={() => setIsImageLoaded(true)} />
+      <div className="sidebar__sport">
+        <button onClick={onSportClick} className={classNames('sidebar__btn', { open: isOpen })}>
+          <span className="sidebar__icon">
+            {!isImageLoaded && <div className='sidebar__icon_skeleton'><BallSkeleton /></div>}
+            <span className="img-container">
+              <img src={sportIcon} alt='' onLoad={() => setIsImageLoaded(true)} />
+            </span>
           </span>
-        </span>
-        <span className="sidebar__name">
-          {data.sportName}
-        </span>
-        {isDesktop && (
-          <span className="sidebar__quantity">
-            {data.eventsCount}
+          <span className="sidebar__name">
+            {data.sportName}
           </span>
-        )}
-      </button>
+          {isDesktop && (
+          <span className="sidebar__info">
+            <span className="sidebar__quantity">
+              {data.eventsCount}
+            </span>
+            <span className="sidebar__more">
+              <svg className="sidebar__svg">
+                <use xlinkHref="#arrow-bottom" />
+              </svg>
+            </span>
+          </span>
+          )}
+        </button>
+      </div>
+      {isOpen && <div className="sidebar__country">
+        <ul className="sidebar__countryList">
+          <li className="sidebar__countryItem">
+            <button onClick={onCountryClick} className={classNames('sidebar__countryButton', { active: isSelected })}>
+              <div className="country__title">
+                <div className="sidebar__countryFlag">
+                  <Flag code={'EN'} />
+                </div>
+                <div className="sidebar__countryName">England</div>
+              </div>
+              <span className="sidebar__countryInfo">
+                <div className="sidebar__countryQuantity">200</div>
+                <span className="sidebar__countryMore">
+                  <svg className="sidebar__countrySvg">
+                    <use xlinkHref="#arrow-bottom" />
+                  </svg>
+                </span>
+              </span>
+            </button>
+            {isSelected && (
+            <div className="sidebar__ligue">
+              <ul className="sidebar__ligueList">
+                <li className="sidebar__ligueItem active">
+                  <div className="sidebar__ligueName">Premier League</div>
+                  <div className="sidebar__ligueQuantity">200</div>
+                </li>
+                <li className="sidebar__ligueItem">
+                  <div className="sidebar__ligueName">Premier League</div>
+                  <div className="sidebar__ligueQuantity">200</div>
+                </li>
+                <li className="sidebar__ligueItem">
+                  <div className="sidebar__ligueName">Premier League</div>
+                  <div className="sidebar__ligueQuantity">200</div>
+                </li>
+              </ul>
+            </div>
+            )}
+          </li>
+        </ul>
+      </div>}
     </li>
   );
 };
