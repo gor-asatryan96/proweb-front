4c5e319a1412c914d1b1f0d18ed270a495e0358a
Lev <tovmasyan.lev@gmail.com>
2022-06-09 17:10:36 +0400

    Feat: prematch events header skeleton

--- src/template/pages/Sportsbook/Sport/pages/Prematch/components/PrematchEvents/components/PrematchEventsHeader/PrematchEventsHeader.jsx
+++ src/template/pages/Sportsbook/Sport/pages/Prematch/components/PrematchEvents/components/PrematchEventsHeader/PrematchEventsHeader.jsx
@@ -1,25 +1,38 @@
 import classNames from 'classnames';
 import { useMediaQuery } from 'react-responsive';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { MEDIA_QUERIES } from '../../../../../../../../../../constants/mediaQuery.constants';
 import s from './PrematchEventsHeader.module.scss';
 import Flag from '../../../../../../../../../../components/Flag/Flag';
 import { selectIsPrematchEventsLoading, selectPrematchActiveLeague } from '../../../../../../../../../../redux/reducers/sport/sport.slice';
 import PrematchEventsHeaderSkeleton from './PrematchEventsHeaderSkeleton';
 import PrematchEventsFilters from '../PrematchEventsFilters/PrematchEventsFilters';
+import { SPORT_TABS } from '../../../../../../constants/sport.constants';
+import { useSportParams } from '../../../../../../hooks/sport.hooks';
+
+const { PRE_MATCH } = SPORT_TABS;
 
 const PrematchEventsHeader = () => {
+  const navigate = useNavigate();
+  const { sportName } = useSportParams();
   const isDesktop = useMediaQuery(MEDIA_QUERIES.DESKTOP);
   const leagueInfo = useSelector(selectPrematchActiveLeague);
   const isEventsLoading = useSelector(selectIsPrematchEventsLoading);
 
   if (!leagueInfo || isEventsLoading) return <PrematchEventsHeaderSkeleton />;
 
+  const { eventsCount, country, competitionName } = leagueInfo;
+
+  const onBackClick = () => {
+    navigate(`/Sport/${PRE_MATCH}/${sportName}`);
+  };
+
   return (
     <div className='bet-filter__block'>
       <div className={s.prematchHeader}>
         {!isDesktop && <div className={classNames(s.prematchHeader__back, { [s.mobile]: !isDesktop })}>
-          <div className={s.prematchHeaderBack__button}>
+          <div onClick={onBackClick} className={s.prematchHeaderBack__button}>
             <svg className={s.prematchHeaderBack__svg}>
               <use xlinkHref="#menu-arrow-left" />
             </svg>
@@ -27,12 +40,12 @@ const PrematchEventsHeader = () => {
         </div>}
         <div className={classNames(s.prematchHeader__info, { [s.mobile]: !isDesktop })}>
           <div className={s.prematchHeader__flag}>
-            <Flag code={'EN'} />
+            <Flag country={country} />
           </div>
-          <div className={s.prematchHeader__country}>England</div>
-          <div className={s.prematchHeader__league}>Premer Legaue</div>
+          <div className={s.prematchHeader__country}>{country}</div>
+          <div className={s.prematchHeader__league}>{competitionName}</div>
         </div>
-        <div className={s.prematchHeader__count}>15</div>
+        <div className={s.prematchHeader__count}>{eventsCount}</div>
       </div>
       <PrematchEventsFilters />
     </div>
--- src/template/pages/Sportsbook/Sport/pages/Prematch/components/PrematchEvents/components/PrematchEventsHeader/PrematchEventsHeaderSkeleton.jsx
+++ src/template/pages/Sportsbook/Sport/pages/Prematch/components/PrematchEvents/components/PrematchEventsHeader/PrematchEventsHeaderSkeleton.jsx
@@ -1,5 +1,52 @@
-const PrematchHeaderSkeleton = () => (
-  <div>PrematchHeaderSkeleton</div>
-);
+import classNames from 'classnames';
+import { useMediaQuery } from 'react-responsive';
+import { MEDIA_QUERIES } from '../../../../../../../../../../constants/mediaQuery.constants';
+import RowSkeleton from '../../../../../../../../../components/Skeletons/RowSkeleton/RowSkeleton';
+import s from './PrematchEventsHeader.module.scss';
 
-export default PrematchHeaderSkeleton;
+const PrematchEventsHeaderSkeleton = () => {
+  const isDesktop = useMediaQuery(MEDIA_QUERIES.DESKTOP);
+  return (
+    <div className={classNames('bet-filter__block', s.skeleton)}>
+      <div className={s.prematchHeader}>
+        {!isDesktop && <div className={classNames(s.prematchHeader__back, { [s.mobile]: !isDesktop })}>
+          <div className={s.prematchHeaderBack__button}>
+            <svg className={s.prematchHeaderBack__svg}>
+              <use xlinkHref="#menu-arrow-left" />
+            </svg>
+          </div>
+        </div>}
+        <div className={classNames(s.prematchHeader__info, { [s.mobile]: !isDesktop })}>
+          <div className={s.prematchHeader__flag}>
+            <RowSkeleton />
+          </div>
+          <div className={s.prematchHeader__country}>
+            <RowSkeleton />
+          </div>
+          <div className={s.prematchHeader__league}>
+            <RowSkeleton />
+          </div>
+        </div>
+        <div className={s.prematchHeader__count}>
+          <RowSkeleton />
+        </div>
+      </div>
+      <div className="bet-filter__more">
+        <div className={classNames('bet-filter__select', s.skeletonSelect)}>
+          <div><RowSkeleton /></div>
+          <div><RowSkeleton /></div>
+        </div>
+        <div className="bet-filter__sides">
+          <div className={s.skeletonMarketType}>
+            <RowSkeleton />
+          </div>
+          <div className={s.skeletonMarketType}>
+            <RowSkeleton />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrematchEventsHeaderSkeleton;
